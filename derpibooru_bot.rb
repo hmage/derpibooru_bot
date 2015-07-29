407d326ce1ef1ea5eccff27f4b096efead22ad21
Eugene Bujak <hmage@hmage.net>
2015-07-29 03:16:22 +0300

    Try the fancy OO approach

--- derpibooru_bot.rb
+++ derpibooru_bot.rb
@@ -2,89 +2,85 @@
 
 # gem install telegram-bot-ruby
 require 'telegram/bot'
+require 'httparty' # telegram/bot is using it anyway, so let's use it too
 require 'yaml'
-require 'net/http'
-require 'awesome_print'
-require 'open-uri'
-require 'erb'
-include ERB::Util
+require 'pp'
 
 config_filename = "settings.yaml"
-file_contents = YAML::load_file(config_filename)
+file_contents = YAML.load_file(config_filename)
 raise "Config file #{config_filename} is empty" if file_contents == false
-loaded = Hash[file_contents.map { |k, v| [k.to_sym, v] }]
-
-telegram_token = loaded[:telegram_token]
-derpibooru_key = loaded[:derpibooru_key]
+telegram_token = file_contents['telegram_token']
+derpibooru_key = file_contents['derpibooru_key']
 ## TODO: check if empty
 
-def respond_not_found(bot, message)
-    bot.api.sendMessage(chat_id: message.chat.id, text: "I am sorry, #{message.from.first_name}, no images to reply with")
-end
+class DerpibooruBot
+    include HTTParty
+    format :json
 
-def fetch_and_post_image(bot, message, url, section)
-    response = Net::HTTP.get_response URI.parse(url)
-
-    ## TODO: add error checks
-
-    data = JSON.parse(response.body)
-    entry = data[section][0]
-    post_image(bot, message, entry)
-end
-
-def post_image(bot, message, entry)
-    if entry == nil
-        respond_not_found(bot, message)
-        return
+    def initialize(bot, derpibooru_key = nil)
+        @bot = bot
+        @derpibooru_key = derpibooru_key
     end
 
-    ## TODO: add error checks
-    image_url = URI.parse(entry["representations"]["tall"])
-    image_url.scheme = "https" if image_url.scheme == nil
-    ap entry
-
-    save_filename = "/tmp/#{entry['id']}.#{entry['original_format']}"
-    source_url = "https://derpibooru.org/#{entry['id_number']}"
-
-    # download the image
-    open(image_url.to_s) do |f|
-        File.open(save_filename, "wb") do |file| file.puts f.read end
+    def respond_not_found(message)
+        @bot.api.sendMessage(chat_id: message.chat.id, text: "I am sorry, #{message.from.first_name}, got no images to reply with.")
     end
 
-    apiresponse = bot.api.sendPhoto(chat_id: message.chat.id, photo: File.new(save_filename), caption: source_url)
-end
+    def post_image(message, entry)
+        respond_not_found(message) && return if entry == nil
+        image_url = URI.parse(entry["representations"]["tall"])
+        image_url.scheme = "https" if image_url.scheme == nil
+        pp entry
 
-def derpi_gettop(bot, message, derpibooru_key = nil)
-    url = "https://derpibooru.org/lists/top_scoring.json"
-    url << "?key=#{derpibooru_key}" if derpibooru_key != nil
-    fetch_and_post_image(bot, message, url, "images")
-end
+        response = HTTParty.get(image_url)
 
-def derpi_search(bot, message, derpibooru_key = nil)
-    search_term = message.text.split(' ')[1..-1].join(' ')
-    if search_term == ""
-        derpi_gettop(bot, message, derpibooru_key)
-        return
-    end
-    search_term << ", explicit" if derpibooru_key != nil
-    search_term << ", safe" if derpibooru_key == nil
-    search_term << ", -gore"
-    search_term_encoded = url_encode(search_term)
-    url = "https://derpibooru.org/search.json?q=#{search_term_encoded}"
-    url << "&key=#{derpibooru_key}" if derpibooru_key != nil
-    fetch_and_post_image(bot, message, url, "search")
-end
-
-
-Telegram::Bot::Client.run(telegram_token) do |bot|
-    bot.listen do |message|
-        case message.text
-        when /^\/(clop)\b/
-            derpi_search(bot, message, derpibooru_key)
-        when /^\/(pony|derpi)\b/
-            derpi_search(bot, message)
-        when '/toppony'
-            derpi_gettop(bot, message)
+        Tempfile.open(["#{entry['id']}", ".#{entry['original_format']}"]) do |f|
+            f.write response.parsed_response
+            f.rewind
+            apiresponse = @bot.api.sendPhoto(chat_id: message.chat.id, photo: f, caption: "https://derpibooru.org/#{entry['id_number']}")
         end
+    end
+
+    def posttop(message, is_nsfw)
+        post_image(message, gettop(is_nsfw)[0])
+    end
+
+    def gettop(is_nsfw = false)
+        url = "https://derpibooru.org/lists/top_scoring.json"
+        url << "?key=#{@derpibooru_key}" if is_nsfw
+        data = DerpibooruBot.get(url)
+        return data["images"]
+    end
+
+    def respond(message, is_nsfw = false)
+        search_term = message.text.split(' ')[1..-1].join(' ')
+        if search_term == ""
+            posttop(message, is_nsfw)
+            return
+        end
+        entries = search(search_term, is_nsfw)
+        post_image(message, entries[0])
+    end
+
+    def search(search_term, is_nsfw = false)
+        search_term << ", explicit" if is_nsfw
+        search_term << ", safe" if !is_nsfw
+        search_term << ", -gore"
+        search_term_encoded = CGI.escape(search_term)
+        url = "https://derpibooru.org/search.json?q=#{search_term_encoded}"
+        url << "&key=#{@derpibooru_key}" if @derpibooru_key != nil
+        data = DerpibooruBot.get(url)
+        return data["search"]
+    end
+end
+
+bot = Telegram::Bot::Client.new(telegram_token)
+derpibooru_bot = DerpibooruBot.new(bot, derpibooru_key)
+bot.listen do |message|
+    case message.text
+    when /^\/(clop)\b/
+        derpibooru_bot.respond(message, true)
+    when /^\/(pony)\b/
+        derpibooru_bot.respond(message, false)
     end
 end
