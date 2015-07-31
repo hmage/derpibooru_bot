64c5753510f1ff94d683d94b15364abb3b294d3a
Eugene Bujak <hmage@hmage.net>
2015-07-31 15:11:11 +0000

    /ynope is alias for /ynop

--- derpibooru_bot.rb
+++ derpibooru_bot.rb
@@ -144,7 +144,7 @@ while true
                 derpibooru_bot.pony(message, true)
             when /^\/pony\b/
                 derpibooru_bot.pony(message)
-            when /^\/ynop\b/
+            when /^\/ynope?\b/
                 derpibooru_bot.ynop(message)
             when /^\/(start|help)\b/
                 derpibooru_bot.sendtext(message, "Hello! I'm a bot by @hmage that sends you images of ponies.\n\nTo get a random top scoring picture: /pony\n\nTo search for Celestia: /pony Celestia\n\nYou get the idea :)")
