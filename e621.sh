d3c1364d4ad2b1fa8b1605968e01f531b380f974
Eugene Bujak <hmage@hmage.net>
2019-10-04 05:16:59 +0300

    run scripts -- add restart marker

--- e621.sh
+++ e621.sh
@@ -4,5 +4,6 @@ OURDIR="${BASH_SOURCE%/*}"
 cd "$OURDIR"
 while true; do
     ./e621_bot.rb
+    echo ============== restart ==============
     sleep 1
 done
--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ OURDIR="${BASH_SOURCE%/*}"
 cd "$OURDIR"
 while true; do
     ./derpibooru_bot.rb
+    echo ============== restart ==============
     sleep 1
 done
